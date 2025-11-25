565ece374fc7e86e579b5cbdbee85b1d73cb81eb
Alvaro-Santana <147865498+Alvaro-Santana@users.noreply.github.com>
2025-11-25 14:46:18 -0300

    Update seno.js

--- public/seno.js
+++ public/seno.js
@@ -8,23 +8,44 @@ function perguntarUnidade() {
 
     const isGrau = confirm("O valor está em GRAUS? (OK = graus, Cancelar = radianos)");
     let x = parseFloat(ang);
-    let xRad = 0;
+    let xRad, xGraus;
+
+    const doisPi = 2 * Math.PI;
 
     if (isGrau) {
         // GRAUS → RAD
         xRad = x * (Math.PI / 180);
     } else {
         // RADIANOS
-        const doisPi = 2 * Math.PI;
 
+        // Caso 1: Valor pequeno → usar direto
         if (Math.abs(x) <= doisPi) {
-            // Pequeno → NÃO REDUZ
             xRad = x;
-        } else {
-            // Grande → REDUZ
+        } 
+        
+        // Caso 2: Valor grande → reduzir
+        else {
             xRad = x % doisPi;
         }
+
+        // Agora CONVERTER SEMPRE PARA GRAUS
+        xGraus = xRad * (180 / Math.PI);
+
+        // E CONVERTER DE VOLTA PARA RAD
+        xRad = xGraus * (Math.PI / 180);
     }
 
     calcularTaylorSeno(xRad);
+}
+
+function calcularTaylorSeno(x) {
+    const termo1 = x;
+    const termo2 = -(Math.pow(x, 3) / 6);
+    const termo3 = (Math.pow(x, 5) / 120);
+    const termo4 = -(Math.pow(x, 7) / 5040);
+
+    const resultado = termo1 + termo2 + termo3 + termo4;
+
+    document.getElementById("resultado").innerHTML =
+        "Resultado (Taylor ordem 7):<br><strong>" + resultado + "</strong>";
 }
