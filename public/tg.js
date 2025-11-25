0daacdb176d3759a537b7cff1807d05878f4b406
Alvaro-Santana <147865498+Alvaro-Santana@users.noreply.github.com>
2025-11-25 13:46:43 -0300

    Update tg.js

--- public/tg.js
+++ public/tg.js
@@ -1,46 +1,43 @@
-function perguntarUnidade() {
-    const ang = document.getElementById("angulo").value;
 
-    if (ang === "") {
-        alert("Digite um valor!");
-        return;
-    }
-
-    const isGrau = confirm("O valor está em GRAUS? (OK = graus, Cancelar = radianos)");
-
-    let x = parseFloat(ang);
-    let xRad = 0;
-
-    if (isGrau) {
-        xRad = x * (Math.PI / 180);
-    } else {
-        const grauReducao = ((x * 100) % (2 * Math.PI * 100)) / 100;
-        xRad = grauReducao * (Math.PI / 180);
-    }
-
-    calcularTaylorTangente(xRad);
+function reduzirGrausSeNecessario(graus) {
+    if (Math.abs(graus) > 360) return graus % 360;
+    return graus;
 }
 
-function taylorSeno(x) {
-    return x
-        - (Math.pow(x, 3) / 6)
-        + (Math.pow(x, 5) / 120)
-        - (Math.pow(x, 7) / 5040);
+function senoTaylor7(x) {
+    x = reducir(x);
+    const r = x * (Math.PI / 180);
+    return r - r ** 3 / 6 + r ** 5 / 120 - r ** 7 / 5040;
 }
 
-function taylorCosseno(x) {
-    return 1
-        - (Math.pow(x, 2) / 2)
-        + (Math.pow(x, 4) / 24)
-        - (Math.pow(x, 6) / 720);
+function cosTaylor7(x) {
+    x = reducir(x);
+    const r = x * (Math.PI / 180);
+    return 1 - r ** 2 / 2 + r ** 4 / 24 - r ** 6 / 720;
 }
 
-function calcularTaylorTangente(x) {
-    const seno = taylorSeno(x);
-    const cosseno = taylorCosseno(x);
+function reducir(x) {
+    if (Math.abs(x) > 360) return x % 360;
+    return x;
+}
 
-    const resultado = seno / cosseno;
+function tanTaylor7(graus) {
+    graus = reducir(graus);
+    return senoTaylor7(graus) / cosTaylor7(graus);
+}
+
+function tanNormal(graus) {
+    graus = reducir(graus);
+    return Math.tan(graus * Math.PI / 180);
+}
+
+function calcularTan() {
+    let entrada = parseFloat(document.getElementById("valor").value);
+
+    const resultadoTaylor = tanTaylor7(entrada);
+    const resultadoNormal = tanNormal(entrada);
 
     document.getElementById("resultado").innerHTML =
-        "Resultado (Taylor ordem 7):<br><strong>" + resultado + "</strong>";
+        `<p><b>Taylor(7):</b> ${resultadoTaylor}</p>
+         <p><b>Math.tan:</b> ${resultadoNormal}</p>`;
 }
